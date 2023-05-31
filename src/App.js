6716fd5d57cda4dffc05b800adfef397674f2c75
apurva-kuranjekar <apurwakuranjekar100@gmail.com>
2023-05-31 16:22:17 +0530

    added product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components.js/Navbar';
+import Product from './components.js/Product';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Product/>
       
     </div>
   )
---
+++ src/components.js/Product.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <>
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Work Time</h1>
+        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+      </div>
+      
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://id-files.apjonlinecdn.com/landingpages/category-family/hp-laptops-family/images/carousel/w100_generic_laptop_carousel_desktop2023.jpg" alt="content"/>
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        </div>
+      </div>
+    
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://id-files.apjonlinecdn.com/landingpages/category-family/hp-laptops-family/images/carousel/w100_generic_laptop_carousel_desktop2023.jpg" alt="content"/>
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://id-files.apjonlinecdn.com/landingpages/category-family/hp-laptops-family/images/carousel/w100_generic_laptop_carousel_desktop2023.jpg" alt="content"/>
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          
+      </div>
+    
+    </div>
+  </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Product
